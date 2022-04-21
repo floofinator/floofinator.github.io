3ca6716f8cd5c7d89deed11f1c309313ea592b7b
floofinator <lukenthomson9@gmail.com>
2022-04-21 15:25:09 -0400

    Glowy cool layout
    :)

--- squiggleOverlay.js
+++ squiggleOverlay.js
@@ -11,77 +11,99 @@ function include(file) {
 
 include('https://git.io/perlin.js')
 
-class DriftingPoint{
-    constructor(x,y,driftx,drifty){
+class Point{
+    constructor(x,y){
         this.x = x;
         this.y = y;
-        this.driftx = driftx;
-        this.drifty = drifty;
+    }
+}
+
+class DriftingPoint extends Point{
+    constructor(x,y,velocityX,velocityY){
+        super(x,y);
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
     }
     drift() {
-        this.x += this.driftx;
-        this.y += this.drifty;
+        this.x += this.velocityX;
+        this.y += this.velocityY;
     }
-    accelerate(){
-        var divisor = drag+1;
-        this.driftx /= divisor;
-        this.drifty /= divisor;
+    accelerate(multiplier){
+        this.velocityX *= multiplier;
+        this.velocityY *= multiplier;
     }
 }
-var canvas = document.getElementById("overlay");
+class DriftingLine{
+    constructor(color,width,length,velocity,multiplier,windX,windY,windChange){
+        this.wind = new DriftingPoint(windX,windY,windChange,windChange);
+        this.pointList = [];
+        this.color = color;
+        this.length = length;
+        this.width = width;
+        this.velocity = velocity;
+        this.multiplier = multiplier;
+    }
+    addPoint(point){
+        const perlinX = (point.x/canvas.clientWidth)*scale + this.wind.x;
+        const perlinY = (point.y/canvas.clientHeight)*scale + this.wind.y;
+        const velocityX = perlin.get(perlinX,perlinY)*this.velocity;
+        const velocityY = perlin.get(perlinX+0.5,perlinY+0.5)*this.velocity;
+        var newPoint = new DriftingPoint(point.x,point.y,velocityX,velocityY);
+        this.pointList.push(newPoint);
+        if(this.pointList.length > this.length)
+            this.pointList.shift();
+    }
+    drift(){
+        this.wind.drift();
+        for (let index = 0; index < this.pointList.length; index++) {
+            const point = this.pointList[index];
+            point.drift();
+            point.accelerate(this.multiplier);
+        }
+    }
+    draw(){
+        context.lineJoin = "round";
+        context.lineCap = "round";
+        context.lineWidth = this.width;
+        context.beginPath();
+        context.strokeStyle = this.color;
+
+        for (let index = 1; index < this.pointList.length; index++) {
+            const point = this.pointList[index];
+            const previous = this.pointList[index-1];
+            context.moveTo(previous.x,previous.y);
+            context.lineTo(point.x,point.y);
+        }
+        context.stroke();
+
+    }
+}
+var canvas = document.getElementById("squiggleCanvas");
 var context = canvas.getContext("2d");
-var mousePosition = new DriftingPoint(0,0);
-var wind = new DriftingPoint(0,0,0.0125,0.0125);
-const length = 2048;
+
+var mousePoint = new DriftingPoint(0,0);
+
 var scale = 0.125;
-var drift = 2;
-var drag = 0.0125;
-var pointList = [];
+
 window.onresize = updateOverlaySize;
-window.onmousemove = function(e){mousePosition = new DriftingPoint(e.clientX,e.clientY);};
+window.onmousemove = function(e){mousePoint = new DriftingPoint(e.clientX,e.clientY);};
 window.onmousedown = function(e){updateOverlaySize()};
-function addPoint(){
-    const perlinX = (mousePosition.x/canvas.clientWidth)*scale + wind.x;
-    const perlinY = (mousePosition.y/canvas.clientHeight)*scale + wind.y;
-    const driftx = perlin.get(perlinX,perlinY)*drift;
-    const drifty = perlin.get(perlinX+0.25,perlinY+0.25)*drift;
-    newPoint = new DriftingPoint(mousePosition.x,mousePosition.y,driftx,drifty);
-    pointList.push(newPoint);
-    if(pointList.length > length)
-    pointList.shift();
-}
-function driftLine(pointList){
-    for (let index = 0; index < pointList.length; index++) {
-        const point = pointList[index];
-        point.drift();
-        point.accelerate();
-    }
-}
-function drawLine(pointList){
-    context.lineJoin = "round";
-    context.lineCap = "round";
-    context.lineWidth = 2;
-    context.beginPath();
-    context.strokeStyle = "#FFFFFF"
-    for (let index = 1; index < pointList.length; index++) {
-        const point = pointList[index];
-        const previous = pointList[index-1];
-        context.moveTo(previous.x,previous.y);
-        context.lineTo(point.x,point.y);
-    }
-    context.stroke();
-}
+
+var lines = [];
+
 function updateOverlaySize(){
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 }
 function clearOverlay(){context.clearRect(0, 0, canvas.width, canvas.height);}
+
 function updateOverlay(){
     clearOverlay();
-    wind.drift();
-    addPoint(mousePosition);
-    driftLine(pointList);
-    drawLine(pointList);
+    lines.forEach(line => {
+        line.addPoint(mousePoint);
+        line.drift();
+        line.draw();
+    });
 }
 updateOverlaySize();
 var interval = setInterval(updateOverlay,20);
