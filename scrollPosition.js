2de9f6b1803bb6de822eb68f39e079fe0c15af4b
floofinator <lukenthomson9@gmail.com>
2022-04-20 21:22:23 -0400

    CHANGED EVETYIONTRHS
    -YEYEAEHAE

--- scrollPosition.js
+++ scrollPosition.js
@@ -2,6 +2,18 @@ window.addEventListener("scroll", function(){
     var scroll = window.scrollY;
     var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight;
     var scrollpos = document.getElementById("scroll_position");
-    scrollpos.textContent=scroll;
-    scrollpos.style.top=(scroll/limit)*(window.innerHeight-scrollpos.clientHeight*2)+"px";
+
+    var value = (scroll/limit);
+    scrollpos.innerText="⇧";
+
+    var positionRange = (window.innerHeight-scrollpos.clientHeight*2);
+    scrollpos.style.top=value*positionRange+"px";
 });
+
+function topOfPage(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
