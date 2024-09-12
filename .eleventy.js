ce3fd0dce99aca98bb444b6281ad77cf4105ce63
floofinator <lukenthomson9@gmail.com>
2024-09-12 16:52:19 -0400

    CNAME passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/style.css");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/Resume.pdf");
+    eleventyConfig.addPassthroughCopy("CNAME");
 
     eleventyConfig.addFilter("contains", (sub = "", parent = "") => {
         if (parent.length == 1 && sub != parent) return false
