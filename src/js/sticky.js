64a55b6d15969489d4c2d2c2c3441e52258e941c
floofinator <lukenthomson9@gmail.com>
2025-05-01 16:44:59 -0400

    landing page

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -13,13 +13,3 @@ function CheckSticky() {
 }
 
 window.addEventListener("scroll", CheckSticky)
-
-//show
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toggle('show', entry.isIntersecting);
-  })
-})
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el))
